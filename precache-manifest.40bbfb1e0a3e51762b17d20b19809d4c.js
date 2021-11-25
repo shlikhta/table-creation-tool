43450c3e0f43809db6b916af5e4eace7f85ff910
Anton Shlikhta <shlikhta.anton@gmail.com>
2021-11-25 10:17:52 +0200

    Updates

--- precache-manifest.40bbfb1e0a3e51762b17d20b19809d4c.js
+++ precache-manifest.40bbfb1e0a3e51762b17d20b19809d4c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "689b7f5d99b0f57cbff0",
+    "revision": "67324c30cb920a0733ea",
     "url": "/table-creation-tool/static/css/main.fe974e9e.chunk.css"
   },
   {
-    "revision": "689b7f5d99b0f57cbff0",
-    "url": "/table-creation-tool/static/js/main.689b7f5d.chunk.js"
+    "revision": "67324c30cb920a0733ea",
+    "url": "/table-creation-tool/static/js/main.67324c30.chunk.js"
   },
   {
     "revision": "531bb299d84d59bd0df1",
@@ -16,12 +16,8 @@ self.__precacheManifest = [
     "url": "/table-creation-tool/static/js/runtime~main.9d6a558b.js"
   },
   {
-    "revision": "9d4de650604a42feb25097e7a6cede46",
-    "url": "/table-creation-tool/static/media/HelveticaNowText-Medium.9d4de650.woff2"
-  },
-  {
-    "revision": "cd416f754438a9d6e4fbf354bb1dcecd",
-    "url": "/table-creation-tool/static/media/HelveticaNowText-Regular.cd416f75.woff2"
+    "revision": "696ce458a2ad5fbfe219cdb109f4dc58",
+    "url": "/table-creation-tool/static/media/HelveticaNowText-Medium.696ce458.woff"
   },
   {
     "revision": "82732a2bf3d4d5661e781bb5b74ebf89",
@@ -32,8 +28,12 @@ self.__precacheManifest = [
     "url": "/table-creation-tool/static/media/HelveticaNowText-Regular.52e9c7db.eot"
   },
   {
-    "revision": "696ce458a2ad5fbfe219cdb109f4dc58",
-    "url": "/table-creation-tool/static/media/HelveticaNowText-Medium.696ce458.woff"
+    "revision": "9d4de650604a42feb25097e7a6cede46",
+    "url": "/table-creation-tool/static/media/HelveticaNowText-Medium.9d4de650.woff2"
+  },
+  {
+    "revision": "cd416f754438a9d6e4fbf354bb1dcecd",
+    "url": "/table-creation-tool/static/media/HelveticaNowText-Regular.cd416f75.woff2"
   },
   {
     "revision": "32ebfd35129d4ed1ddfa2413109b28aa",
@@ -44,27 +44,27 @@ self.__precacheManifest = [
     "url": "/table-creation-tool/static/media/HelveticaNowText-Medium.dcc4ea63.ttf"
   },
   {
-    "revision": "008a67c49aef4efac3f6244bac6f371c",
-    "url": "/table-creation-tool/static/media/HelveticaNowText-Light.008a67c4.woff2"
-  },
-  {
     "revision": "21a3a47495c9e8751965625063407be6",
     "url": "/table-creation-tool/static/media/HelveticaNowText-Regular.21a3a474.ttf"
-  },
-  {
-    "revision": "46754ffb829242dbeed2cf6c88287be9",
-    "url": "/table-creation-tool/static/media/HelveticaNowText-Light.46754ffb.eot"
   },
   {
     "revision": "43f6366d885a37dacf037d09792f4dee",
     "url": "/table-creation-tool/static/media/HelveticaNowText-Light.43f6366d.woff"
   },
   {
+    "revision": "46754ffb829242dbeed2cf6c88287be9",
+    "url": "/table-creation-tool/static/media/HelveticaNowText-Light.46754ffb.eot"
+  },
+  {
+    "revision": "008a67c49aef4efac3f6244bac6f371c",
+    "url": "/table-creation-tool/static/media/HelveticaNowText-Light.008a67c4.woff2"
+  },
+  {
     "revision": "a977ced76499525fcdbae799286a3560",
     "url": "/table-creation-tool/static/media/HelveticaNowText-Light.a977ced7.ttf"
   },
   {
-    "revision": "c2699d72b6a07b81f3b7bde684336fb8",
+    "revision": "723b70be1381cbdfd24134e7f6b662c5",
     "url": "/table-creation-tool/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/table-creation-tool/precache-manifest.083b223565ec514dc040eb0edeaa1219.js"
+  "/table-creation-tool/precache-manifest.40bbfb1e0a3e51762b17d20b19809d4c.js"
 );
 
 workbox.clientsClaim();
